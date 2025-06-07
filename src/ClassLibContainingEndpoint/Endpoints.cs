87dc971b34497b2bcf747cb87c0bdc16902ba76c
Matt Parker <61717342+MattParkerDev@users.noreply.github.com>
2025-06-07 15:26:45 +1000

    Initial commit

---
+++ src/ClassLibContainingEndpoint/Endpoints.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+
+namespace ClassLibContainingEndpoint;
+
+public static class Endpoints
+{
+	public static void MapEndpoints(this RouteGroupBuilder builder)
+	{
+		// increment the number and notice that the generated openapi document does not change without a rebuild
+		builder.MapGet("MyEndpoint1", () => "Hello, World!").WithName("MyEndpoint");
+	}
+}
---
+++ src/WebApi/Program.cs
@@ -0,0 +1,22 @@
+using ClassLibContainingEndpoint;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+var baseRoute = app.MapGroup("").WithTags("Api");
+baseRoute.MapEndpoints();
+
+await app.RunAsync();
